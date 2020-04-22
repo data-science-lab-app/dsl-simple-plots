61ebe9bd4e2dad86ffb140128951774ff4d4a839
CobeGreene <cobegreene@gmail.com>
2020-04-21 21:28:48 -0700

    Scatter plot implemented and tested

---
+++ lib/scatter.plot.spec.ts
@@ -0,0 +1,110 @@
+import { ScatterPlot } from "./scatter.plot"
+
+describe('Scatter Plot Tests', () => {
+    let visualization: ScatterPlot;
+
+    beforeEach(() => {
+        visualization = new ScatterPlot();
+    });
+
+    it('options should return false for no more', () => {
+        expect(visualization.getOptions().noMore()).toBeFalsy();
+    });
+
+    it('options should return three options', () => {
+        expect(visualization.getOptions().options().length).toBe(3);
+    });
+
+    it('submit should be no more afterwards', () => {
+        visualization.getOptions().submit({
+            title: 'My title',
+            color: 'red'
+        });
+
+        expect(visualization.getOptions().noMore()).toBeTruthy();
+    });
+
+    it('inputs should return two inputs', () => {
+        expect(visualization.getInputs().inputs().length).toBe(2);
+    });
+
+    it('visualize should set data with title', () => {
+        visualization.getOptions().submit({
+            title: 'My title',
+            color: 'red'
+        });
+        visualization.getInputs().submit({
+            x: {
+                features: ['x label'],
+                examples: [[1],[2],[3],[4],[5]]
+            },
+            y: {
+                features: ['y label'],
+                examples: [[1],[2],[3],[4],[5]]
+            },
+        });
+        expect(visualization.data).toEqual({
+            x: [1,2,3,4,5],
+            y: [1,2,3,4,5],
+            xLabel: 'x label',
+            yLabel: 'y label',
+            title: 'My title',
+            color: 'red'
+        });
+        expect(visualization.getOptions().noMore()).toBeTruthy();
+    });
+    
+    it('visualize should set data without title', () => {
+        visualization.getOptions().submit({
+            color: 'red'
+        });
+        visualization.getInputs().submit({
+            x: {
+                features: ['x label'],
+                examples: [[1],[2],[3],[4],[5]]
+            },
+            y: {
+                features: ['y label'],
+                examples: [[1],[2],[3],[4],[5]]
+            },
+        });
+        expect(visualization.data).toEqual({
+            x: [1,2,3,4,5],
+            y: [1,2,3,4,5],
+            xLabel: 'x label',
+            yLabel: 'y label',
+            color: 'red',
+            title: undefined
+        });
+        expect(visualization.getOptions().noMore()).toBeTruthy();
+    });
+    
+    it('visualize should set data with hex color', () => {
+        visualization.getOptions().submit({
+            color: 'red',
+            hexColor: '#000000'
+        });
+        visualization.getInputs().submit({
+            x: {
+                features: ['x label'],
+                examples: [[1],[2],[3],[4],[5]]
+            },
+            y: {
+                features: ['y label'],
+                examples: [[1],[2],[3],[4],[5]]
+            },
+        });
+        expect(visualization.data).toEqual({
+            x: [1,2,3,4,5],
+            y: [1,2,3,4,5],
+            xLabel: 'x label',
+            yLabel: 'y label',
+            color: '#000000',
+            title: undefined
+        });
+        expect(visualization.getOptions().noMore()).toBeTruthy();
+    });
+
+
+});
+
---
+++ lib/scatter.plot.ts
@@ -0,0 +1,250 @@
+import { VisualizationPlugin, PluginInputs, PluginOptions, Option, PluginDataInput, PluginData, TextOption, ChoicesOption } from 'data-science-lab-core';
+import { ColorChoices } from './colors';
+
+interface ScatterPlotInput {
+    x: number[];
+    y: number[];
+    xLabel: string;
+    yLabel: string;
+    color: string;
+    title?: string;
+}
+
+export class ScatterPlot extends VisualizationPlugin {
+
+    options: ScatterPlotPluginOptions;
+    inputs: ScatterPlotPluginInputs;
+
+    data: ScatterPlotInput;
+
+    constructor() {
+        super();
+        this.options = new ScatterPlotPluginOptions(this);
+        this.inputs = new ScatterPlotPluginInputs(this);
+
+        this.data = {} as ScatterPlotInput;
+    }
+
+    getOptions(): PluginOptions {
+        return this.options;
+    }
+    getInputs(): PluginInputs {
+        return this.inputs;
+    }
+
+    visualization(): string {
+        return `
+        <!DOCTYPE html>
+        <html>
+        
+        <head>
+            <meta charset="utf-8">
+        
+            <!-- Load d3.js -->
+            <script src="https://d3js.org/d3.v4.js"></script>
+        
+            <!-- Create a div where the graph will take place -->
+            <style>
+                body {
+                    overflow: hidden;
+                }
+            </style>
+        </head>
+        
+        <body>
+            <div id="chart"></div>
+            <script type="text/javascript">
+                var id = '#chart';
+                var xvalues = ${JSON.stringify(this.data.x)};
+                var yvalues = ${JSON.stringify(this.data.y)};
+                var color = ${JSON.stringify(this.data.color)};
+                var xlabel = ${JSON.stringify(this.data.xLabel)};
+                var ylabel = ${JSON.stringify(this.data.yLabel)};
+                var title = ${this.data.title !== undefined ? JSON.stringify(this.data.title) : 'undefined'};
+        
+                if (title !== undefined) {
+                    var margin = { top: 60, right: 50, bottom: 80, left: 50 },
+                        width = 1020 - margin.left - margin.right,
+                        height = 780 - margin.top - margin.bottom;
+                } else {
+                    var margin = { top: 10, right: 50, bottom: 80, left: 50 },
+                        width = 1020 - margin.left - margin.right,
+                        height = 780 - margin.top - margin.bottom;
+                }
+        
+        
+                var svg = d3.select(id)
+                    .append('svg')
+                    .attr('width', '100vw')
+                    .attr('height', '100vh')
+                    .attr('viewBox', '0 0 ' + (width + margin.left + margin.right) + ' ' + (height + margin.top + margin.bottom))
+                    .attr('preserveAspectRatio', 'xMidYMid meet')
+                    .append('g')
+                    .attr('transform',
+                        'translate(' + margin.left + ',' + margin.top + ')');
+        
+                var x = d3.scaleLinear()
+                    .domain([d3.min(xvalues), d3.max(xvalues)])
+                    .range([0, width]);
+        
+                svg.append('g')
+                    .attr('transform', 'translate(0,' + height + ')')
+                    .call(d3.axisBottom(x));
+        
+                var y = d3.scaleLinear()
+                    .range([height, 0])
+                    .domain([d3.min(yvalues), d3.max(yvalues)]);
+        
+                svg.append('g')
+                    .call(d3.axisLeft(y));
+        
+                var data = d3.entries(yvalues);
+        
+                svg.append('g')
+                    .selectAll('dot')
+                    .data(data)
+                    .enter()
+                    .append("circle")
+                    .attr("cx", function (d, i) { return x(xvalues[i]) })
+                    .attr("cy", function (d, i) { return y(yvalues[i]) })
+                    .attr("r", 5)
+                    .attr("fill", color);
+        
+                svg.append('text')
+                    .attr('text-anchor', 'middle')
+                    .attr('transform', 'rotate(-90)')
+                    .attr('y', -margin.left + 20)
+                    .attr('x', -(margin.top / 2) - height / 2 + 10)
+                    .text(ylabel);
+        
+                svg.append('text')
+                    .attr('text-anchor', 'middle')
+                    .style("font-size", "16px")
+                    .attr('x', (width / 2))
+                    .attr('y', height + margin.bottom  / 2)
+                    .text(xlabel);
+        
+                if (title !== undefined) {
+                    svg.append("text")
+                        .attr("x", (width / 2))
+                        .attr("y", 0 - (margin.top / 2))
+                        .attr("text-anchor", "middle")
+                        .style("font-size", "30px")
+                        .style("text-decoration", "underline")
+                        .text(title);
+                }
+            </script>
+        </body>`;
+    }
+
+    setX(points: number[], label: string) {
+        this.data.x = points;
+        this.data.xLabel = label;
+    }
+
+    setY(points: number[], label: string) {
+        this.data.y = points;
+        this.data.yLabel = label;
+    }
+
+    setColor(color: string) {
+        this.data.color = color;
+    }
+
+    setTitle(title: string) {
+        this.data.title = title;
+    }
+
+}
+
+class ScatterPlotPluginInputs extends PluginInputs {
+    constructor(public visualization: ScatterPlot) {
+        super();
+    }
+
+    submit(inputs: { [id: string]: PluginData; }): void {
+        if (inputs['x'] === undefined) {
+            throw new Error(`Scatter Plot's submit expecting plugin data with key x`);
+        } else {
+            this.visualization.setX(
+                inputs['x'].examples.map(value => value[0]),
+                inputs['x'].features[0]);
+        }
+
+        if (inputs['y'] === undefined) {
+            throw new Error(`Scatter Plot's submit expecting plugin data with key y`);
+        } else {
+            this.visualization.setY(
+                inputs['y'].examples.map(value => value[0]),
+                inputs['y'].features[0]);
+        }
+    }
+
+    inputs(): PluginDataInput[] {
+        return [
+            {
+                id: 'x',
+                label: 'X Points',
+                min: 1,
+                max: 1,
+                type: 'number'
+            },
+            {
+                id: 'y',
+                label: 'Y Points',
+                min: 1,
+                max: 1,
+                type: 'number'
+            },
+        ];
+    }
+}
+
+class ScatterPlotPluginOptions extends PluginOptions {
+    state: number;
+
+    constructor(public visualization: ScatterPlot) {
+        super();
+        this.state = 1;
+    }
+
+    submit(inputs: { [id: string]: any; }): void {
+        if (inputs['title'] !== '') {
+            this.visualization.setTitle(inputs['title']);
+        }
+        if (inputs['hexColor'] !== undefined && inputs['hexColor'] !== '') {
+            this.visualization.setColor(inputs['hexColor']);
+        } else {
+            this.visualization.setColor(inputs['color']);
+        }
+        this.state = 2;
+    }
+    options(): Option[] {
+        return [
+            new TextOption({
+                id: 'title',
+                label: 'Provide a title (if you wish to include one)',
+                min: 0,
+            }),
+            new TextOption({
+                id: 'hexColor',
+                label: 'Type a color in hex (example: #00bfff)',
+                pattern: '^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$',
+                min: 0,
+                max: 7
+            }),
+            new ChoicesOption({
+                id: 'color',
+                label: 'Select a color',
+                choices: ColorChoices
+            })
+        ];
+    }
+
+    noMore(): boolean {
+        return this.state === 2;
+    }
+
+}
+
+
